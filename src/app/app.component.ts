155a189c36aeb4067a5db5d8e7db382623099d14
DaedalusAlpha <mattrmcc@gmail.com>
2022-09-28 23:48:22 -0500

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/favicon.ico 	modified:   src/index.html 	modified:   src/styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,11 @@ export class AppComponent {
     category: 'Dinner',
     price: 11.99,
   };
-  item2: MenuItem = { item: 'Pizza', category: 'Dinner', price: 11.99 };
-  item3: MenuItem = { item: 'Wings', category: 'Sides', price: 8.99 };
-  item4: MenuItem = { item: 'Breadsticks', category: 'Sides', price: 4.99 };
-  item5: MenuItem = { item: 'Caesar Salad', category: 'Salads', price: 5.99 };
-  item6: MenuItem = { item: 'Cinnamon Roll', category: 'Dessert', price: 8.99 };
+  item2: MenuItem = { item: 'pizza', category: 'dinner', price: 11.99 };
+  item3: MenuItem = { item: 'wings', category: 'sides', price: 8.99 };
+  item4: MenuItem = { item: 'breadsticks', category: 'sides', price: 4.99 };
+  item5: MenuItem = { item: 'caesar salad', category: 'salads', price: 5.99 };
+  item6: MenuItem = { item: 'cinnamon roll', category: 'dessert', price: 8.99 };
   menu: MenuItem[] = [
     this.item1,
     this.item2,
