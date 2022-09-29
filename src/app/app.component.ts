2753fdea291bcd96886a435f3601f071e79374cf
DaedalusAlpha <mattrmcc@gmail.com>
2022-09-29 17:49:08 -0500

    modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,36 @@ import { MenuItem } from './menu-item';
 })
 export class AppComponent {
   title = 'Chirpus Pizza';
-  item1: MenuItem = {
-    item: 'Chicken Fingers',
-    category: 'Dinner',
-    price: 11.99,
-  };
-  item2: MenuItem = { item: 'pizza', category: 'dinner', price: 11.99 };
-  item3: MenuItem = { item: 'wings', category: 'sides', price: 8.99 };
-  item4: MenuItem = { item: 'breadsticks', category: 'sides', price: 4.99 };
-  item5: MenuItem = { item: 'caesar salad', category: 'salads', price: 5.99 };
-  item6: MenuItem = { item: 'cinnamon roll', category: 'dessert', price: 8.99 };
   menu: MenuItem[] = [
-    this.item1,
-    this.item2,
-    this.item3,
-    this.item4,
-    this.item5,
-    this.item6,
+    {
+      item: 'Chicken Fingers',
+      category: 'Dinner',
+      price: 11.99,
+    },
+    {
+      item: 'pizza',
+      category: 'dinner',
+      price: 11.99,
+    },
+    {
+      item: 'wings',
+      category: 'sides',
+      price: 8.99,
+    },
+    {
+      item: 'breadsticks',
+      category: 'sides',
+      price: 4.99,
+    },
+    {
+      item: 'caesar salad',
+      category: 'salads',
+      price: 5.99,
+    },
+    {
+      item: 'cinnamon roll',
+      category: 'dessert',
+      price: 8.99,
+    },
   ];
 }
