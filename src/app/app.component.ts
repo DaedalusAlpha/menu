e9c8eb31c97e0effa69f681a450028efeb41c1f9
DaedalusAlpha <mattrmcc@gmail.com>
2022-09-28 21:09:40 -0500

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,14 @@ export class AppComponent {
   title = 'Chirpus Pizza';
   item1: MenuItem = {
     item: 'Chicken Fingers',
-    category: 'dinner',
+    category: 'Dinner',
     price: 11.99,
   };
-  item2: MenuItem = { item: 'Pizza', category: 'dinner', price: 11.99 };
-  item3: MenuItem = { item: 'Wings', category: 'sides', price: 8.99 };
-  item4: MenuItem = { item: 'Breadsticks', category: 'sides', price: 4.99 };
-  item5: MenuItem = { item: 'Caesar Salad', category: 'salads', price: 5.99 };
-  item6: MenuItem = { item: 'Cinnamon Roll', category: 'dessert', price: 8.99 };
+  item2: MenuItem = { item: 'Pizza', category: 'Dinner', price: 11.99 };
+  item3: MenuItem = { item: 'Wings', category: 'Sides', price: 8.99 };
+  item4: MenuItem = { item: 'Breadsticks', category: 'Sides', price: 4.99 };
+  item5: MenuItem = { item: 'Caesar Salad', category: 'Salads', price: 5.99 };
+  item6: MenuItem = { item: 'Cinnamon Roll', category: 'Dessert', price: 8.99 };
   menu: MenuItem[] = [
     this.item1,
     this.item2,
